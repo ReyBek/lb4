02c037aba755bdb3f711b938f99a02ee52d33c9d
ReyBek <musaid794@gmail.com>
2022-02-01 21:58:49 +0300

    end

---
+++ doc + pyProj/individual.py
@@ -0,0 +1,9 @@
+from math import sqrt
+a = float(input('a='))
+b = float(input('b='))
+d = abs(a)
+c = abs(d)
+print("Среднее арифметическое: ")
+print((d + c)/2)
+print("Среднее геометрическое ")
+print(sqrt(d*c))
